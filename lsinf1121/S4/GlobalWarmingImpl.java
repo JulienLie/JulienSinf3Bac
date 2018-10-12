085b9040fe9ad504e21573df6c35d6274d4b1912
Lienard Julien <julien.lienard@student.uclouvain.bee>
2018-10-12 09:52:25 +0200

    Algo S4

--- lsinf1121/S4/GlobalWarmingImpl.java
+++ lsinf1121/S4/GlobalWarmingImpl.java
@@ -3,41 +3,52 @@ import java.util.*;
 public class GlobalWarmingImpl extends GlobalWarming {
 
   public static void main(String[] args){
-    int [][] tab = new int[][] {{1,3,3,1,3},
-                          {4,2,2,4,5},
-                          {4,4,1,4,2},
-                          {1,4,2,3,6},
-                          {1,1,1,6,3}};
+    Random rand = new Random();
+    int n = rand.nextInt(10);
+    int [][] tab = new int[n][n];
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < n; j++){
+        tab[i][j] = rand.nextInt(7);
+        System.out.print(tab[i][j] + "\t");
+      }
+      System.out.println();
+    }
     GlobalWarming gw = new GlobalWarmingImpl(tab);
-    System.out.println(gw.nbSafePoints(2));
+    n = rand.nextInt(7);
+    System.out.println("point for " + n + ":" + gw.nbSafePoints(n));
   }
 
-  private final TreeMap<Integer> list;
+  private final int[] list;
 
     public GlobalWarmingImpl(int[][] altitude) {
         super(altitude);
-        list = new TreeMap<>();
-        for(int[] tab : altitude){
-          for(int i : tab){
-            Integer nbr = list.get(i);
-            if(nbr == null){
-              list.put(i, 1);
-            }
-            else{
-              list.replace(i, nbr+1);
-            }
+        list = new int[altitude.length*altitude.length];
+        for(int i = 0; i < altitude.length; i++){
+          for(int j = 0; j < altitude.length; j++){
+            list[i+(altitude.length*j)] = altitude[i][j];
           }
         }
+        Arrays.sort(list);
     }
 
 
     public int nbSafePoints(int waterLevel) {
-        SortedMap<Integer> sub = list.headMap(waterLevel);
-        int sum = 0;
-        for(Integer i : sub.values()){
-          sum += i;
+      if(list.length == 0){
+        return 0;
+      }
+      if(waterLevel < 0){
+            return this.list.length;
         }
-        return sum;
+        else if(waterLevel >= list[list.length-1]){
+          return 0;
+        }
+      int bin = -1;
+      for(int i = 1; bin < 0; i++){
+        bin = Arrays.binarySearch(this.list, waterLevel+i);
+      }
+      while(bin >= 0 && list[bin] > waterLevel){
+        bin--;
+      }
+      return this.list.length-bin-1;
     }
-
 }
